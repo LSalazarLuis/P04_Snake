720fb86eae85b07ecde445d4a9fa26ec0813729a
LSalazarL <luis.salazar.ing@gmail.com>
2024-07-11 11:08:40 -0400

    Se agrega icono

--- src/principal/GameFrame.java
+++ src/principal/GameFrame.java
@@ -1,5 +1,7 @@
 package principal;
 
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 public class GameFrame extends JFrame{
@@ -8,6 +10,7 @@ public class GameFrame extends JFrame{
 
         add(new GamePanel());
         setTitle("SnakeGame");
+        setIconImage(Toolkit.getDefaultToolkit().getImage("src/assets/icono2.png"));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
         pack();
